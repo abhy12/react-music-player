5619ac59af31e9bff648a3faf55fc8a467bad238
Abhishek <98876719+abhy12@users.noreply.github.com>
2023-10-27 17:51:21 +0530

    add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx}",
    ],
    theme: {
-      extend: {},
+      extend: {
+         colors: {
+            'primary-blue': '#0816bf'
+         }
+      },
    },
    plugins: [],
 }
