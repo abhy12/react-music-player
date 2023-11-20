3d3bc87ad9ec628d82a9fa2d787828fecc053086
Abhishek <98876719+abhy12@users.noreply.github.com>
2023-11-20 16:28:55 +0530

    debounce search

--- src/components/filter/Search.tsx
+++ src/components/filter/Search.tsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { useAppDispatch, updateSearch } from "../../store/music-store";
+import { debounce } from "../../../util/util";
 
 interface SearchProps{
    className?: string,
@@ -8,6 +9,8 @@ interface SearchProps{
 
 export default function Search( { className }: SearchProps ) {
    const dispatch = useAppDispatch();
+   // @ts-ignore
+   const updateSearchTerms = debounce( ( e: Event ) => dispatch( updateSearch( e.target?.value ) ), 500 );
 
    return(
       <div className={`${className ? className + ' ' : ''}relative rounded-full border border-primary-blue overflow-hidden text-lg`}>
@@ -15,9 +18,7 @@ export default function Search( { className }: SearchProps ) {
             className="block font-semibold w-full py-2 pl-4 pr-11 h-11 bg-transparent focus:outline-none"
             type="text"
             placeholder="Search"
-            onChange={( e ) => {
-               dispatch( updateSearch( e.currentTarget.value ) );
-            }}
+            onChange={updateSearchTerms}
          />
          <button className="bg-primary-blue text-white absolute right-0 top-0 bottom-0 h-full aspect-square rounded-full">
             <FontAwesomeIcon icon={faChevronRight} />
